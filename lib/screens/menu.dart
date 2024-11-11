a1652aea29ec8e1bb14dae938197a771b0005c93
gracekarinn <gracekarina12@gmail.com>
2024-11-11 11:48:44 +0700

    feat: add main menu screen with product viewing and adding options

---
+++ lib/screens/menu.dart
@@ -0,0 +1,168 @@
+import 'package:flutter/material.dart';
+import 'package:takhor_mobile/widgets/left_drawer.dart';
+import 'package:takhor_mobile/screens/product_form.dart';
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: const IconThemeData(color: Colors.white),
+        title: const Text(
+          'Takhor',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        backgroundColor: Colors.indigo,
+      ),
+      drawer: const LeftDrawer(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: <Widget>[
+              const Padding(
+                padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+                child: Text(
+                  'Selamat datang di Takhor!',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              GridView.count(
+                primary: true,
+                padding: const EdgeInsets.all(20),
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+                crossAxisCount: 3,
+                shrinkWrap: true,
+                children: <Widget>[
+                  Material(
+                    color: Colors.indigo,
+                    borderRadius: BorderRadius.circular(10),
+                    child: InkWell(
+                      onTap: () {
+                        ScaffoldMessenger.of(context)
+                          ..hideCurrentSnackBar()
+                          ..showSnackBar(const SnackBar(
+                              content: Text(
+                                  "Kamu telah menekan tombol Lihat Produk!")));
+                      },
+                      child: Container(
+                        padding: const EdgeInsets.all(8),
+                        child: const Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(
+                                Icons.list_alt,
+                                color: Colors.white,
+                                size: 30.0,
+                              ),
+                              Padding(
+                                padding: EdgeInsets.all(3.0),
+                                child: Text(
+                                  "Lihat Produk",
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Material(
+                    color: Colors.indigo,
+                    borderRadius: BorderRadius.circular(10),
+                    child: InkWell(
+                      onTap: () {
+                        ScaffoldMessenger.of(context)
+                          ..hideCurrentSnackBar()
+                          ..showSnackBar(const SnackBar(
+                              content: Text(
+                                  "Kamu telah menekan tombol Tambah Produk!")));
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => ProductForm(),
+                          ),
+                        );
+                      },
+                      child: Container(
+                        padding: const EdgeInsets.all(8),
+                        child: const Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(
+                                Icons.add_shopping_cart,
+                                color: Colors.white,
+                                size: 30.0,
+                              ),
+                              Padding(
+                                padding: EdgeInsets.all(3.0),
+                                child: Text(
+                                  "Tambah Produk",
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Material(
+                    color: Colors.indigo,
+                    borderRadius: BorderRadius.circular(10),
+                    child: InkWell(
+                      onTap: () {
+                        ScaffoldMessenger.of(context)
+                          ..hideCurrentSnackBar()
+                          ..showSnackBar(const SnackBar(
+                              content:
+                                  Text("Kamu telah menekan tombol Logout!")));
+                      },
+                      child: Container(
+                        padding: const EdgeInsets.all(8),
+                        child: const Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(
+                                Icons.logout,
+                                color: Colors.white,
+                                size: 30.0,
+                              ),
+                              Padding(
+                                padding: EdgeInsets.all(3.0),
+                                child: Text(
+                                  "Logout",
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
